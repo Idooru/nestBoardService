8185fbdf8de11605f2474100f679c41e904c5d5b
Idooru <shere1765@gmail.com>
2022-05-26 16:12:27 +0900

    0.5.2.6

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -1,6 +1,7 @@
-import { Logger, NestMiddleware } from "@nestjs/common";
+import { Logger, NestMiddleware, Injectable } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
+@Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger("HTTP");
   use(req: Request, res: Response, next: NextFunction) {
--- src/model/auth/jwt/jwt.starategy.ts
+++ src/model/auth/jwt/jwt.starategy.ts
@@ -1,6 +1,9 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { PassportStrategy } from "@nestjs/passport";
 import { UserRepository } from "../../user/user.repository";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userRepository: UserRepository) {
     super({
